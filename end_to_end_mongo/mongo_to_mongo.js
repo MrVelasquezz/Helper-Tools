70cf82e19ed6e0eeca4daece8e955a875b725e1a
MrVelasquezz <smyrnijoswest51@gmail.com>
2023-05-04 04:18:23 +0200

    Some fixes in end_to_end mongo

--- end_to_end_mongo/mongo_to_mongo.js
+++ end_to_end_mongo/mongo_to_mongo.js
@@ -15,15 +15,38 @@ class CopyEndToEnd {
         try {
             console.log("Connecting to 'to' database")
             this.client = new mongo(this.to)
-            this.client.connect()
+            await this.client.connect()
             const mongoClient = this.client.db(this.db)
-            const inserted = await mongoClient.collection(this.collection)
+            /*const inserted = await mongoClient.collection(this.collection)
                 .insertMany(this.data)
             if (inserted && inserted.insertedCount) {
                 console.log("Inserted %d of %d elements", inserted.insertedCount, this.data.length)
             } else {
                 throw new NoElementsInDb("No elements were inserted")
+            }*/
+            /*let updated_c = 0 
+            for(const data of this.data){
+                const updated = await mongoClient.collection(this.collection)
+                    .updateOne({land_code: data.land_code}, {$set: data}, {upsert: true})
+                
+                if(updated && updated.modifiedCount){
+                    updated_c++
+                }
             }
+            console.log("Updated %d of %d elements", updated_c, this.data.length)*/
+            const existing = await mongoClient.collection(this.collection)
+                .find({}).toArray()
+            let inserted_c = 0
+            for(let i = 0; i < this.data.length; i++){
+                if(!existing.find(r => r.land_code == this.data[i].land_code)){
+                    const inserted = await mongoClient.collection(this.collection)
+                        .insertOne(this.data[i]) 
+                    if(inserted && inserted.acknowledged){
+                        inserted_c++
+                    }
+                }
+            }
+            console.log("Inserted %d of %d elements", inserted_c, this.data.length)
         } catch (e) {
             console.dir(e)
         } finally {
@@ -41,10 +64,10 @@ class CopyEndToEnd {
         try {
             console.log("Connecting to 'from' database")
             this.client = new mongo(this.from)
-            this.client.connect()
+            await this.client.connect()
             const mongoClient = this.client.db(this.db)
             const result = await mongoClient.collection(this.collection)
-                .find({}).toArray()
+                .find({}, {projection: {_id: 0}}).toArray()
             if (result && result.length) {
                 console.log("Found %d elements", result.length)
                 this.data = result
